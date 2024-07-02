15e1afac4e3fe74e9fffd983ffe3a978d768a29e
Renan Calusa <63789983+renan-calusa@users.noreply.github.com>
2024-07-02 12:59:15 -0300

    Update genius.c

--- genius.c
+++ genius.c
@@ -1,9 +1,7 @@
-#define MAXROUND 20
-
-
 // Variaveis globais e blibiotecas
 bool game_over = false;
 int* sequence;
+int MAXROUND = 3;
 int rodada = 1;
 int leds[] = {2, 3, 4, 5}; // Saidas digitais dos Leds
 int buttons[] = {8, 9, 10, 11}; // Saidas digitais dos botoes
@@ -13,7 +11,20 @@ void setup() {
   
   // Inicializar o vetor de sequencia e popular com numeros aleatorios [0, 3]
   sequence = (int*) malloc(sizeof(int)*MAXROUND);
+  randomSeed(analogRead(0));
   for (int i=0; i<MAXROUND; i++) sequence[i] = random(4);
+
+  // Config dos leds
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  // Config dos botoes
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
 }
 
 
@@ -28,20 +39,19 @@ void loop() {
       for (int i=0; i<4; i++) {
     
         digitalWrite(leds[i], HIGH);
-        delay(300);
+        delay(100);
         digitalWrite(leds[i], LOW);
-        delay(300);
+        delay(100);
       }
     }
-    
-    free(sequence);
-    return;
+
+    reset_game();
   }
 
   // Verificar fim de jogo por resposta incorreta
   else if (game_over) {
-    free(sequence);
-    return;
+
+    reset_game();
   }
 
   // Mostra a sequencia do proximo round
@@ -60,9 +70,9 @@ void showSequence() {
   for (int i=0; i<rodada; i++) {
   
     digitalWrite(leds[sequence[i]], HIGH); // Liga o led atual
-    delay(1000);
+    delay(500);
     digitalWrite(leds[sequence[i]], LOW); // Desliga o led atual
-    delay(1000);
+    delay(500);
   }
 }
 
@@ -79,12 +89,13 @@ void waitPlayer() {
       // Verificar qual botao foi pressionado
       for (int k=0; k<4; k++) {
       
-        if (digitalRead(buttons[k] == HIGH)) {
+        if ((digitalRead(buttons[k]) == HIGH)) {
           
           // k guarda qual botao foi pressionado
           digitalWrite(leds[k], HIGH);
-          delay(500);
+          delay(200);
           digitalWrite(leds[k], LOW);
+          delay(600);
           
           notPlay = false;
           answer = k;
@@ -103,8 +114,8 @@ void checkPlay(int guess, int rodada) {
 
   if (sequence[rodada] != guess) {
     
-    // Caso que errou - pisca todos os leds 3 vezes
-    for (int i=0; i<3; i++) {
+    // Caso que errou - pisca todos os leds 5 vezes
+    for (int i=0; i<5; i++) {
     
       for (int k=0; k<4; k++) digitalWrite(leds[k], HIGH);
       delay(100);
@@ -118,4 +129,14 @@ void checkPlay(int guess, int rodada) {
   }
   
   // Caso acerte, apenas vai para o proximo round
+}
+
+
+void reset_game() {
+
+  randomSeed(analogRead(0));
+  for (int i=0; i<MAXROUND; i++) sequence[i] = random(4);
+  rodada = 1;
+  game_over = false;
+  delay(2000);
 }
